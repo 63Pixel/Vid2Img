c410ead317079a06d1a8e85e3bf27e4770635d7b
Stefan Nitz <129666921+63Pixel@users.noreply.github.com>
2023-04-26 09:08:15 +0200

    Update Vid2Img.py
    Added changes:
    Selection of source files
    Custom name for export

--- Vid2Img.py
+++ Vid2Img.py
@@ -16,7 +16,7 @@ def process_video(video_file_path, frame_interval, use_gpu=False, img_format='pn
             frame = cv2.cuda_GpuMat(frame)
 
         if frame_count % frame_interval == 0:
-            img_file_path = os.path.join(export_folder, f"frame{image_count:04d}.{img_format}")
+            img_file_path = os.path.join(export_folder, f"{output_name}_{image_count:04d}.{img_format}")
 
             if use_gpu:
                 frame = frame.download()
@@ -29,21 +29,19 @@ def process_video(video_file_path, frame_interval, use_gpu=False, img_format='pn
 
     cap.release()
 
-
 # Get the script's directory
 script_dir = os.path.dirname(os.path.realpath(__file__))
 
-# Find the first video file in the script's directory
-video_extensions = ('.mp4', '.avi', '.mov', '.mkv', '.flv')
-video_file_path = None
-for file in os.listdir(script_dir):
-    if file.lower().endswith(video_extensions):
-        video_file_path = os.path.join(script_dir, file)
-        break
+# Prompt the user for the video file name
+video_file_name = input("Please enter the video file name to process: ")
+video_file_path = os.path.join(script_dir, video_file_name)
 
-if not video_file_path:
-    print("No video file found in the script's directory.")
+if not os.path.isfile(video_file_path):
+    print(f"Video file '{video_file_name}' not found in the script's directory.")
     exit()
+
+# Prompt the user for the output name
+output_name = input("Please enter the output name for the exported frames: ")
 
 # Prompt the user for the frame interval in FPS, whether to use GPU, and the image format
 frame_interval = int(input("Please enter the frame interval in FPS: "))
